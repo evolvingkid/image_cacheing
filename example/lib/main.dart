b8a7a33987c3710ba82640149fb564b0d516c838
evolvingkid <fayezmohammed23@gmail.com>
2021-05-31 12:03:56 +0530

    modify example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,18 +1,43 @@
-import 'package:example/screens/home.dart';
 import 'package:flutter/material.dart';
+import 'package:image_cacheing/image_cacheing.dart';
+import './global/vars.dart' as global;
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.blue),
       home: HomePage(),
+    );
+  }
+}
+
+// ? example of the image cacheing
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // * normal Image cache
+            Container(child: ImageCacheing(url: global.imageUrl)),
+            // * image cache with loading
+            // ? who can pass any type widget to show on loading widgets
+            Container(
+                child: ImageCacheing(
+                    url: global.imageLoadingUrl,
+                    loadingWidget: Center(child: CircularProgressIndicator()))),
+            // * image cache with warning
+            // ? who can pass any type widget to show on error widgets
+            Container(
+                child: ImageCacheing(
+                    url: global.imageErrorUrl,
+                    errorWidget: Center(child: Icon(Icons.warning)))),
+          ],
+        ),
+      ),
     );
   }
 }
